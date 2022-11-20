f0acdfc53387180a2c896b836198406e7c47fafb
islahtelu <puddyislah22@gmail.com>
2022-11-20 11:48:38 +0700

    add function saklar

--- js/cf.js
+++ js/cf.js
@@ -1,31 +1,46 @@
 // contact form
 const scriptURL = "https://script.google.com/macros/s/AKfycbzjtBADhCZtH-y3-gXn9uwaJv3i1D0l52DEW1-9MXUIk74Lr-K0iGn3vbjjmuhyELNexg/exec";
-const form = document.forms["submit-to-google-sheet"]
-const btnKirim = document.querySelector('.btn-kirim')
-const btnLoading = document.querySelector('.btn-loading')
-const alertContactSuccess = document.querySelector('.alert-contact-success')
-const alertContactFailed = document.querySelector('.alert-contact-failed')
+const form = document.forms["submit-to-google-sheet"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const alertContactSuccess = document.querySelector(".alert-contact-success");
+const alertContactFailed = document.querySelector(".alert-contact-failed");
+
+// sleep time expects milliseconds
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function saklar() {
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+}
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // setelah tombol diklik
-    // tampilkan tombol loading, hilangkan tombol kirim
-    btnLoading.classList.toggle('d-none')
-    btnKirim.classList.toggle('d-none')
-    
-    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  // setelah tombol diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  saklar();
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
-            console.log("Success!", response)
-            btnLoading.classList.toggle('d-none')
-            btnKirim.classList.toggle('d-none')
-            alertContactSuccess.classList.toggle('d-none')
-            form.reset()
-        })
-        .catch((error) => {
-            console.error("Error!", error.message)
-            btnLoading.classList.toggle('d-none')
-            btnKirim.classList.toggle('d-none')
-            alertContactFailed.classList.toggle('d-none')
-        })
+      console.log("Success!", response);
+      saklar();
+      alertContactSuccess.classList.toggle("d-none");
+      // Usage!
+      sleep(4000).then(() => {
+        alertContactSuccess.classList.toggle("d-none");
+      });
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      saklar();
+      alertContactFailed.classList.toggle("d-none");
+
+      sleep(4000).then(() => {
+        alertContactFailed.classList.toggle("d-none");
+      });
+    });
 });
