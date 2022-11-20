f6fb31bf520e07904e38009c48a8492410832d64
islahtelu <puddyislah22@gmail.com>
2022-11-20 18:44:54 +0700

    add alert for pretest

--- js/countdown.js
+++ js/countdown.js
@@ -1,20 +1,25 @@
+const countDown = document.getElementById("countdown");
+const preTest = document.querySelector(".pre-test");
+
 var acara = new Date("Nov 26, 2022 11:00:00").getTime();
 var x = setInterval(function () {
-    var hariini = new Date().getTime();
-    var jarak = acara - hariini;
+  var hariini = new Date().getTime();
+  var jarak = acara - hariini;
 
-    var hari = Math.floor(jarak / (1000 * 60 * 60 * 24));
-    var jam = Math.floor((jarak % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var menit = Math.floor((jarak % (1000 * 60 * 60)) / (1000 * 60));
-    var detik = Math.floor((jarak % (1000 * 60)) / 1000);
+  var hari = Math.floor(jarak / (1000 * 60 * 60 * 24));
+  var jam = Math.floor((jarak % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var menit = Math.floor((jarak % (1000 * 60 * 60)) / (1000 * 60));
+  var detik = Math.floor((jarak % (1000 * 60)) / 1000);
 
-    document.getElementById("hari").innerHTML = hari;
-    document.getElementById("jam").innerHTML = jam;
-    document.getElementById("menit").innerHTML = menit;
-    document.getElementById("detik").innerHTML = detik;
+  document.getElementById("hari").innerHTML = hari;
+  document.getElementById("jam").innerHTML = jam;
+  document.getElementById("menit").innerHTML = menit;
+  document.getElementById("detik").innerHTML = detik;
 
-    if (jarak < 0) {
-        clearInterval(x);
-        document.getElementById("clock").innerHTML = "Selesai";
-    }
+  if (jarak < 0) {
+    clearInterval(x);
+    document.getElementById("text-countdown").innerHTML = "Big Class 3 is Live Now!";
+    countDown.classList.toggle("d-none");
+    preTest.classList.toggle("d-none");
+  }
 }, 1000);
